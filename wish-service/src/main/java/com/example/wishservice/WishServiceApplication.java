de8419149e2a3d7b7990909ef8391dc2309b99f7
basudev7240 <basudev7240@gmail.com>
2018-09-29 11:37:43 +0530

    intital commit

---
+++ wish-service/src/main/java/com/example/wishservice/WishServiceApplication.java
@@ -0,0 +1,12 @@
+package com.example.wishservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WishServiceApplication.class, args);
+	}
+}
