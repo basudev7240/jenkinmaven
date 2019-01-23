e13d034f9627e8d4a4b5336ce3c940ea58dc18c7
basudev7240 <basudev7240@gmail.com>
2019-01-23 21:39:08 +0530

    new api developed name as getData

--- wish-service/src/main/java/com/example/wishservice/ctrl/WishController.java
+++ wish-service/src/main/java/com/example/wishservice/ctrl/WishController.java
@@ -5,12 +5,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/app")
 public class WishController {
 	@GetMapping
 	@RequestMapping("/wish")
 	public String doWish() {
 		
 		return "hi this is boot with maven with jenkins";
+	}
+	
+	@RequestMapping("/data")
+	public String getData() {
+		
+		
+		
+		return "currently no data available";
 	}
 
 }
